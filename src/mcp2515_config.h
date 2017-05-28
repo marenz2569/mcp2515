6c037c2dfb9d6f6ca647a1ebcdcff64f22356656
Markus Schmidl <markus.schmidl@ymail.com>
2017-05-28 14:25:23 +0200

    mcp2515 io functionsn are not used

--- src/mcp2515_config.h
+++ src/mcp2515_config.h
@@ -9,12 +9,12 @@
  * undef to disable output via RX(0|1)BF pins
  * saves 1 byte of ram and 4 bytes of flash
  */
-#define MCP2515_OUTPUT
+//#define MCP2515_OUTPUT
 
 /**
  * undef to disable input via TX(0|1|2)RTS pins
  * saves 1 byte of ram and 4 bytes of flash
  */
-#define MCP2515_INPUT
+//#define MCP2515_INPUT
 
 #endif
